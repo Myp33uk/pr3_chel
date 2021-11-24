1b4bc8746afd3c19b9e5f59befc396a3523a6ac0
мурзек <@DESKTOP-F6R7CD8>
2021-11-24 20:32:34 +0300

    Добавьте файлы проекта.

---
+++ Practice/Practice.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib_14
+{
+    public class Practice
+    {
+        public static void MaximumValueMatrix(int[,] matrix, out string result)
+        {
+            List<int> element = new List<int>();
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                int increment = 1;
+                int diminution = 1;
+                for (int j = 0; j < matrix.GetLength(0) - 1; j++)
+                {
+                    if (matrix[j, i] > matrix[j + 1, i])
+                    {
+                        diminution++;
+                    }
+                    if (matrix[j, i] < matrix[j + 1, i])
+                    {
+                        increment++;
+                    }
+                }
+                if (increment == matrix.GetLength(0))
+                {
+                    element.Add(matrix[matrix.GetLength(0) - 1, i]);
+                }
+                if (diminution == matrix.GetLength(0))
+                {
+                    element.Add(matrix[0, i]);
+                }
+            }
+            if (element.Count == 0)
+            {
+                result = "0";
+            }
+            else
+            {
+                result = element.Max().ToString();
+            }
+        }
+    }
+}
